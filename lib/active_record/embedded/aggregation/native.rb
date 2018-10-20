2f795f925ef0447f7bfc8efb825efffcf93c9caf
Tom Scott <tscott@weblinc.com>
2018-10-20 13:15:27 -0400

    Reduce size of #results in Native aggregation

--- lib/active_record/embedded/aggregation/native.rb
+++ lib/active_record/embedded/aggregation/native.rb
@@ -4,9 +4,16 @@ module ActiveRecord
       class Native < self
         delegate :empty?, to: :results
 
+
+        def query_for(model)
+          model.public_send(as).where(@filters)
+        end
+
         def results
-          @results ||= parent.all.map { |model| [model, model.public_send(as).where(@filters)] }
-                .select { |model, items| items.any? }
+          @results ||= parent
+                        .all
+                        .map { |model| [model, query_for(model)] }
+                        .select { |model, items| items.any? }
         end
       end
     end
