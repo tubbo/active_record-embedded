d2cfbec124b20079ab5c569d13402b5622bbc229
Tom Scott <tscott@weblinc.com>
2018-10-22 09:12:37 -0400

    Add sqlite driver

---
+++ lib/active_record/embedded/aggregation/sqlite.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module ActiveRecord
+  module Embedded
+    class Aggregation
+      # MySQL driver for aggregation queries
+      class Sqlite < Aggregation
+        def results
+          models = filtered_results
+          models.offset(from) unless from.zero?
+          models.limit(to) unless to == -1
+          models.map { |model| [model, query_for(model)] }
+        end
+
+        private
+
+        def filtered_results
+          filters.each_with_object(relation) do |(key, value), criteria|
+            criteria.where("json_extract(?, '$.data[*].?') = ?", as, key, value)
+          end
+        end
+
+        def relation
+          parent.unscoped
+        end
+      end
+    end
+  end
+end
